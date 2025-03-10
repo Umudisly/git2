a52a5bdfa2e90495871b352c92fb0ab2bf864cea
Umudisly <112596403+Umudisly@users.noreply.github.com>
2025-03-09 20:36:47 -0400

    updated provider

--- provider.tf
+++ provider.tf
@@ -8,9 +8,9 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-1"
   access_key = var.Access_key_ID
-  secret_key = var.Secret_access_key 
+  secret_key = var.Secret_access_key
+  region = "us-east-1"
 }
 
 
