38281056ea84297bdb448d4442e5fc2fba3e7136
Umudisly <112596403+Umudisly@users.noreply.github.com>
2025-03-11 14:50:54 -0400

    sly update 3rd

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "terraform-ec2" {
   ami           = var.instance_id
   instance_type = var.type #"t2.micro"
   tags = {
-    Name = "Terraform"
+    Name = "Terraform Code"
   }
 }
 
